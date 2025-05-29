3bde73727a33fd7dec6f38bb81f30f12b071c094
Hikary Muñoz Iida <hmunoz@fi.uba.ar>
2025-05-28 21:43:54 -0300

    Display init

--- app/src/task_menu.c
+++ app/src/task_menu.c
@@ -100,6 +100,7 @@ void task_menu_init(void *parameters)
 	task_menu_st_t	state;
 	task_menu_ev_t	event;
 	bool b_event;
+	char menu_str[DISPLAY_CHAR_WIDTH + 1];
 
 	/* Print out: Task Initialized */
 	LOGGER_LOG("  %s is running - %s\r\n", GET_NAME(task_menu_init), p_task_menu);
@@ -125,16 +126,19 @@ void task_menu_init(void *parameters)
 	b_event = p_task_menu_dta->flag;
 	LOGGER_LOG("   %s = %s\r\n", GET_NAME(b_event), (b_event ? "true" : "false"));
 
+	displayInit( DISPLAY_CONNECTION_GPIO_4BITS );
+
+    // Imprime el texto del menú principal, en dos filas
+    displayCharPositionWrite(0, 0);
+    text_info_motor_in_row(menu_str, 0, motor_cfg + 0);
+    displayStringWrite(menu_str);
+    displayCharPositionWrite(0, 1);
+    text_info_motor_in_row(menu_str, 1, motor_cfg + 1);
+    displayStringWrite(menu_str);
+
 	cycle_counter_init();
 	cycle_counter_reset();
 
-	displayInit( DISPLAY_CONNECTION_GPIO_4BITS );
-
-    displayCharPositionWrite(0, 0);
-	displayStringWrite("TdSE Bienvenidos");
-
-	displayCharPositionWrite(0, 1);
-	displayStringWrite("Test Nro: ");
 
 	g_task_menu_tick_cnt = G_TASK_MEN_TICK_CNT_INI;
 }
@@ -180,9 +184,6 @@ void task_menu_update(void *parameters)
             continue;
 		}
 
-        snprintf(menu_str, sizeof(menu_str), "%lu", (g_task_menu_cnt/1000ul));
-        displayCharPositionWrite(10, 1);
-        displayStringWrite(menu_str);
 
         p_task_menu_dta->tick = DEL_MEN_XX_MAX;
 
@@ -358,11 +359,11 @@ void change_current_cfg(motor_cfg_t * cfg, motor_cfg_type_t type) {
 }
 
 void text_info_motor_in_row(char str[], uint8_t index, motor_cfg_t * cfg) {
-    snprintf(str, DISPLAY_CHAR_WIDTH + 1, "Motor %d %s %d %c",
+    snprintf(str, DISPLAY_CHAR_WIDTH + 1, "Motor %1.1d %s %1.1d %c",
             index,
             (cfg->power) ? "ON" : "OFF",
             cfg->speed,
-            (cfg->spin == LEFT) ? 'L' : 'R');
+            ((cfg->spin == LEFT) ? 'L' : 'R'));
 }
 
 void text_select_motor(char str[], uint8_t index) {
